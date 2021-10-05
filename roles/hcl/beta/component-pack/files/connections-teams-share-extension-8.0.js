213593bce3e86500112cfbd8d9b3c1d223cf6d76
Nitin <nitin.jagjivan@hcl.com>
2021-10-05 11:51:43 +0530

    Remove beta folder

--- roles/hcl/beta/component-pack/files/connections-teams-share-extension-8.0.js
+++
@@ -1,5 +0,0 @@
-function openShareExtension() {
-  let shareUrl =  "https://teams.microsoft.com/share?href=" + window.location.href + "&referrer=" +  window.location.host;
-  window.open(shareUrl,'_blank', 'width=700,height=600'); 
-}
-
