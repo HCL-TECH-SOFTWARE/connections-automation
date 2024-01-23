9eb050fdad217266a0fed3e2e35ebaf9f0b20d62
sabrina-yee <75322195+sabrina-yee@users.noreply.github.com>
2024-01-23 09:42:43 -0500

    Jan CR5 release (#328)

--- roles/third_party/nfs-install/templates/nfsSetupScript/nfsSetup.sh
+++ roles/third_party/nfs-install/templates/nfsSetupScript/nfsSetup.sh
@@ -20,17 +20,4 @@ for VOLUME in $VOLUMES; do
  echo "$VOLUME        ${DEC_IP[0]}.${DEC_IP[1]}.0.0/255.255.0.0(rw,root_squash)" >> /etc/exports
 done
 
-# Enable and start resources for NFS
-systemctl enable rpcbind
-systemctl enable nfs-server
-systemctl enable nfs-lock
-systemctl enable nfs-idmap
-systemctl start rpcbind
-systemctl start nfs-server
-systemctl start nfs-lock
-systemctl start nfs-idmap
-
-# Restart NFS server and configure the firewall
-systemctl restart nfs-server
-
-echo "NFS successfully configured!"
+echo "NFS is ready to be configured."
